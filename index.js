6c8944d55a35a6104d8da3ff8df819c333eda572
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2022-02-05 20:31:49 +0200

    Animate a single column of white letters appearing

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
     const CHARACTERS = {
         /** https://en.wikipedia.org/wiki/Half-width_kana */
         'KATAKANA': [
-            "ｦ", "ｧ", "ｨ", "ｩ", "ｪ", "ｫ", "ｬ", "ｭ", "ｮ", "ｯ", "ｱ", "ｲ", "ｳ", "ｴ",
-            "ｵ", "ｶ", "ｷ", "ｸ", "ｹ", "ｺ", "ｻ", "ｼ", "ｽ", "ｾ", "ｿ", "ﾀ", "ﾁ", "ﾂ",
-            "ﾃ", "ﾄ", "ﾅ", "ﾆ", "ﾇ", "ﾈ", "ﾉ", "ﾊ", "ﾋ", "ﾌ", "ﾍ", "ﾎ", "ﾏ", "ﾐ",
-            "ﾑ", "ﾒ", "ﾓ", "ﾔ", "ﾕ", "ﾖ", "ﾗ", "ﾘ", "ﾙ", "ﾚ", "ﾛ", "ﾜ", "ﾝ"
+            'ｦ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｬ', 'ｭ', 'ｮ', 'ｯ', 'ｱ', 'ｲ', 'ｳ', 'ｴ',
+            'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾁ', 'ﾂ',
+            'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ', 'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ',
+            'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ', 'ﾜ', 'ﾝ'
         ],
         'LATIN': [
-            "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D",
-            "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D',
+            'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'
         ],
         'DIGITS': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     }
@@ -44,36 +44,73 @@
         canvas.height = window.innerHeight
     }
 
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+
     function drawScreenSaver(canvas) {
         const context = canvas.getContext('2d');
-        context.font = '50px monospace'
+        context.font = '30px monospace'
+        context.textAlign = 'start'
+        context.textBaseline = 'top'
 
         context.fillStyle = COLOR_GREEN.DARKEST
-        context.fillText('ｦ', 30, 40)
+        context.fillText('0', 0, 0)
 
         context.fillStyle = COLOR_GREEN.DARKEST
-        context.fillText('B', 30, 80)
+        context.fillText('A', 0, 30)
 
         context.fillStyle = COLOR_GREEN.DARKEST
-        context.fillText('C', 30, 120)
+        context.fillText('B', 0, 60)
 
         context.fillStyle = COLOR_GREEN.DARKEST
-        context.fillText('D', 30, 160)
+        context.fillText('C', 0, 90)
+
+        context.fillStyle = COLOR_GREEN.DARKEST
+        context.fillText('D', 0, 120)
 
         context.fillStyle = COLOR_GREEN.DARKER
-        context.fillText('F', 30, 200)
+        context.fillText('F', 0, 150)
 
         context.fillStyle = COLOR_GREEN.DARK
-        context.fillText('G', 30, 240)
+        context.fillText('G', 0, 180)
 
         context.fillStyle = COLOR_GREEN.MEDIUM
-        context.fillText('H', 30, 280)
+        context.fillText('H', 0, 210)
 
         context.fillStyle = COLOR_GREEN.LIGHTER
-        context.fillText('J', 30, 320)
+        context.fillText('J', 0, 240)
 
         context.fillStyle = COLOR_GREEN.LIGHTEST
-        context.fillText('K', 30, 360)
+        context.fillText('K', 0, 270)
+
+        const characters = [
+            ...CHARACTERS.KATAKANA,
+            ...CHARACTERS.DIGITS,
+            ...CHARACTERS.LATIN,
+        ]
+        const characterCount = characters.length
+
+        const x = 30
+        let y = 0
+        const verticalGap = 30
+
+        const intervalId = setInterval(() => {
+            if (y >= canvas.height) {
+                clearInterval(intervalId)
+                return
+            }
+
+            const number = getRandomNumber(characterCount);
+            const character = characters[number]
+
+            console.log(number)
+
+            context.fillStyle = COLOR_GREEN.LIGHTEST
+            context.fillText(character, x, y)
+
+            y += verticalGap
+        }, 100)
     }
 
     init()
