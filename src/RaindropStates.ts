77ceb9e9a972079db78840e3fd2fab9c0a976bcc
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2022-02-17 17:16:15 +0200

    Feature/3 use typescript (#2)
    * npm install typescript
    
    * tsc --init
    
    * conver from js to ts files
    
    * `tsc` converts from ts to js and runs in the browser
    
    * `npm run start` now watches for changes and retranspiles the code

---
+++ src/RaindropStates.ts
@@ -0,0 +1,16 @@
+export enum RAINDROP_STATES {
+   /** Only used when initializing a variable - should be updated asap. */
+   INITIAL = 'INITIAL',
+
+   /** The character is transitioning from white to matrix green. */
+   FADING_IN = 'FADING_IN',
+
+   /** The character is staying in matrix green (until eol). */
+   ALIVE = 'ALIVE',
+
+   /** The character is transitioning from matrix green to black. */
+   FADING_OUT = 'FADING_OUT',
+
+   /** The character is no longer visible. */
+   DEAD = 'DEAD',
+}
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+/**
+ * ! IMPORTANT !
+ * This is the entrypoint of the application!
+ * If renaming this file - also rename it in index.html!
+ */
+
+import { SETTINGS } from './_settings'
+import { RainColumn } from './RainColumn'
+import { getRandomNumber } from './helpers'
+
+function startApplication() {
+   const canvas = document.getElementById('canvas') as HTMLCanvasElement
+   const context = canvas.getContext('2d', { alpha: false })!
+
+   resizeCanvasToFitScreen(context)
+   configureCanvasFont(context)
+   configureCanvasColors(context)
+   initializeRainColumns(context)
+}
+
+function resizeCanvasToFitScreen(context: CanvasRenderingContext2D) {
+   context.canvas.width = window.innerWidth
+   context.canvas.height = window.innerHeight
+}
+
+function configureCanvasFont(context: CanvasRenderingContext2D) {
+   context.font = `${SETTINGS.CHARACTERS.FONT_SIZE}px monospace`
+   context.textAlign = 'start'
+   context.textBaseline = 'top'
+}
+
+function configureCanvasColors(context: CanvasRenderingContext2D) {
+   context.canvas.style.backgroundColor = SETTINGS.COLORS.DEAD
+}
+
+function initializeRainColumns(context: CanvasRenderingContext2D) {
+   const verticalGap = SETTINGS.CHARACTERS.VERTICAL_GAP
+   const horizontalGap = SETTINGS.CHARACTERS.HORIZONTAL_GAP
+   const paddingLeft = SETTINGS.RAIN.PADDING_LEFT
+   const paddingTop = SETTINGS.RAIN.PADDING_TOP
+
+   const columnCount = Math.floor(context.canvas.width / 30)
+
+   // Initialize the rain columns
+   for (let i = 0; i < columnCount; i++) {
+      const rainColumn = new RainColumn()
+      rainColumn.init(
+         i * horizontalGap + paddingLeft,
+         paddingTop,
+         context.canvas.height,
+         SETTINGS.CHARACTERS.FONT_SIZE,
+         verticalGap,
+      )
+
+      startRainColumn(rainColumn, context)
+   }
+}
+
+/**
+ * After a timeout, start the rain column and update it periodically
+ */
+function startRainColumn(rainColumn: RainColumn, context: CanvasRenderingContext2D) {
+   const randomStartTime = getRandomNumber(SETTINGS.RAIN.RANDOMIZE_START)
+   const randomRefreshTime = getRandomNumber(SETTINGS.RANDOMIZE_REFRESH_TIME) + 50
+
+   setTimeout(
+      () => {
+         const draw = () => {
+            rainColumn.draw(context)
+
+            setTimeout(
+               () => requestAnimationFrame(draw),
+               randomRefreshTime
+            )
+         }
+
+         requestAnimationFrame(draw)
+      },
+      randomStartTime
+   )
+}
+
+startApplication()
